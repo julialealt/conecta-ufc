4d107bab2a32704c2c820d0facac2c784a8705f3
julialealt <julialeal.t@gmail.com>
2025-06-24 09:44:34 -0300

    feat: add logo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+  experimental: {
+    images: {
+      allowFutureImage: true,
+    },
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,18 @@ import Header from "./components/header/Header";
 import Input from "./components/input/input";
 import SearchBar from "./components/searchBar/SearchBar";
 import { Button } from "./components/ui/Button";
+import logoSmall from "../../public/assets/logo_sm.svg";
+import logoLarge from "../../public/assets/logo_lg.svg"
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-start gap-4 p-8">
       <p className="text-violet-50">Componentes</p>
+
+      <Image src={logoSmall} alt="" />
+
+      <Image src={logoLarge} alt="" />
 
       <Button>Button</Button>
 
