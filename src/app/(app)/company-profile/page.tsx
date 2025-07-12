d98d881f9733c3bdb8e512528c4571f53bee4597
julialealt <julialeal.t@gmail.com>
2025-07-12 19:43:13 -0300

    fix: add company profile page

--- src/app/(app)/company-profile/page.tsx
+++ src/app/(app)/company-profile/page.tsx
@@ -52,11 +52,11 @@ export default function CompanyProfilePage() {
               logoUrl={""}
               companyName={"IBM"}
               jobTitle={"UI/UX Designer"}
-              description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release."}
+              description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived..."}
               location={"Remoto"}
               salary={"R$1.200,00"}
               workload={"20h semanais"}
-              buttonText="Aplicar"
+              buttonText="Ver candidatos"
             />
 
             <JobCard
@@ -67,7 +67,7 @@ export default function CompanyProfilePage() {
               location={"Remoto"}
               salary={"R$1.200,00"}
               workload={"20h semanais"}
-              buttonText="Aplicar"
+              buttonText="Ver candidatos"
             />
           </div>
         </div>
