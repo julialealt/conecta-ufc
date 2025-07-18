186bbd96414c32d5a9679c040066bdcfc0b265f3
rafaelFacundo <rafaelfacundo63@gmail.com>
2025-07-18 08:33:37 -0300

    create employer done

--- src/app/sign-up/company/page.tsx
+++ src/app/sign-up/company/page.tsx
@@ -1,42 +1,85 @@
 "use client";
 
-import { useState } from 'react'
-import Image from 'next/image'
-import logo from '../../../../public/assets/logo_lg.svg'
-import { useRouter } from 'next/navigation'
-import { Button } from '@/app/components/ui/button';
-import Input from '@/app/components/ui/input';
-import Select from '@/app/components/ui/select';
+import { useState } from "react";
+import Image from "next/image";
+import logo from "../../../../public/assets/logo_lg.svg";
+import { useRouter } from "next/navigation";
+import { Button } from "@/app/components/ui/Button";
+import Input from "@/app/components/ui/input";
+import Select from "@/app/components/ui/select";
+import DescriptionInput from "@/app/components/ui/description-input";
+import LoadingStatus from "@/app/components/loadingStatus/LoadingStatus";
+import api from "@/services/axios";
 
 export default function CompanySignUpPage() {
-  const router = useRouter()
+  const router = useRouter();
 
-  const [name, setName] = useState('')
-  const [description, setDescription] = useState('')
-  const [profile, setProfile] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [profile, setProfile] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleSignUp = () => {
-    alert(`Cadastro realizado com sucesso!\n\nNome: ${name}\nDescrição: ${description}\nPerfil: ${profile}\nEmail: ${email}`)
+  const handleSignUp = async () => {
+    setIsLoading(true);
+    try {
+      const response = await api.post("/employers/register", {
+        name,
+        description,
+        profile,
+        email,
+        password,
+      });
+
+      console.log(response);
+      if (response.status === 201) {
+        const emailResponse = await api.post("/auth/send-email", {
+          userEmail: email,
+        });
+        if (emailResponse.status === 200) {
+          alert(
+            "Cadastro realizado com sucesso!, por favor clique em ok para confirmar seu email"
+          );
+          router.push(`/sign-up/validate-email/${email}`);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Algo deu errado, tente mais tarde");
+    }
+    setIsLoading(false);
   };
 
   const handleBackToSignIn = () => {
-    router.push('/sign-in')
+    router.push("/sign-in");
   };
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-950 p-8">
+      {isLoading && <LoadingStatus />}
       <div className="w-[390px] inline-flex flex-col justify-start items-start gap-8">
         <div className="self-stretch flex flex-col justify-start items-center gap-3">
           <Image src={logo} alt="Logo ConectaUFC" />
-          <p className="self-stretch text-center justify-start text-violet-50 text-xs font-medium">Conectando talentos, construindo futuros.</p>
+          <p className="self-stretch text-center justify-start text-violet-50 text-xs font-medium">
+            Conectando talentos, construindo futuros.
+          </p>
         </div>
 
         <div className="self-stretch flex flex-col justify-start items-start gap-8">
-          <p className="self-stretch text-center justify-start text-zinc-400 text-sm font-medium leading-[20px]">O futuro começa aqui.<br />Cadastre-se e faça parte da rede de talentos da UFC.</p>
+          <p className="self-stretch text-center justify-start text-zinc-400 text-sm font-medium leading-[20px]">
+            O futuro começa aqui.
+            <br />
+            Cadastre-se e faça parte da rede de talentos da UFC.
+          </p>
 
-          <form className="flex w-full flex-col gap-y-4" onSubmit={(e) => { e.preventDefault(); handleSignUp(); }}>
+          <form
+            className="flex w-full flex-col gap-y-4"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSignUp();
+            }}
+          >
             <Input
               id="name"
               name="name"
@@ -46,7 +89,7 @@ export default function CompanySignUpPage() {
               onChange={(e) => setName(e.target.value)}
             />
 
-            <Input
+            <DescriptionInput
               id="description"
               name="description"
               label="Descrição*"
@@ -62,9 +105,12 @@ export default function CompanySignUpPage() {
               placeholder="Selecione uma opção"
               value={profile}
               options={[
-                { value: 'company', label: 'Empresa ou organização ou recrutador' },
-                { value: 'teacher', label: 'Professor' },
-                { value: 'outro', label: 'Outro' },
+                {
+                  value: "company",
+                  label: "Empresa ou organização ou recrutador",
+                },
+                { value: "teacher", label: "Professor" },
+                { value: "outro", label: "Outro" },
               ]}
               onChange={(e) => setProfile(e.target.value)}
             />
@@ -97,11 +143,7 @@ export default function CompanySignUpPage() {
               >
                 Voltar para o login
               </Button>
-              <Button
-                type="submit"
-                variant="primary"
-                className="w-full"
-              >
+              <Button type="submit" variant="primary" className="w-full">
                 Cadastrar
               </Button>
             </div>
