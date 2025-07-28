e0ca5a0e79b88cdf32c8c1b979223e9aab7e2a95
julialealt <julialeal.t@gmail.com>
2025-07-28 18:21:03 -0300

    refactor: add fetched infos to home page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -5,13 +5,13 @@ import api from "@/services/axios";
 import { Employer, Student } from "@/context/appContext";
 import { InfoCard } from "../components/ui/info-card";
 import { SearchBar } from "../components/ui/search-bar";
+import { Spinner } from "../components/ui/spinner";
 
 export default function Home() {
   const [opportunities, setOpportunities] = useState<Opportunity[]>([]);
   const [students, setStudents] = useState<Student[]>([]);
   const [employers, setEmployers] = useState<Employer[]>([]);
-  const [fetchingOpportunities, setFetchingOpportunities] =
-    useState<boolean>(true);
+  const [fetchingOpportunities, setFetchingOpportunities] = useState<boolean>(true);
   const [fetchingStudents, setFetchingStudents] = useState<boolean>(true);
   const [fetchingEmployers, setFetchingEmployers] = useState<boolean>(true);
 
@@ -58,135 +58,37 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="w-full min-h-screen bg-zinc-950 flex flex-col items-center pt-6 pb-16">
+    <main className="w-full h-screen bg-zinc-950 flex flex-col items-center pt-6 pb-16 overflow-hidden">
       <div className="w-full max-w-[1200px] px-4 md:px-8 flex flex-col items-center gap-12">
         <SearchBar
           placeholder="Pesquisar alunos, empresas ou oportunidades"
           onFilterClick={() => { }}
-          containerClassName="w-full max-w-[1200px] self-stretch"
+          containerClassName="w-full self-stretch"
         />
 
-        <div className="max-w-[1200px] self-stretch inline-flex justify-start items-start gap-12">
+        <div className="self-stretch inline-flex justify-start items-start gap-12">
           <div className="flex-1 self-stretch inline-flex flex-col justify-start items-start gap-8">
             <div className="self-stretch inline-flex flex-col justify-center items-start gap-1">
               <div className="self-stretch justify-start text-violet-50 text-xl font-semibold leading-[150%]">Vagas recentes</div>
               <div className="self-stretch justify-start text-zinc-300 text-sm font-medium leading-[150%]">As últimas oportunidades publicadas na plataforma</div>
             </div>
 
-            <div className="w-full self-stretch inline-flex flex-col justify-start items-start gap-5">
-              <div className="w-full self-stretch inline-flex justify-start items-center gap-5">
-                <InfoCard
-                  title={"UI/UX Designer"}
-                  subtitle={"IBM"}
-                  imageUrl=""
-                  student={false}
-                  href=""
-                  className="w-full"
-                />
-                <InfoCard
-                  title={"UI/UX Designer"}
-                  subtitle={"IBM"}
-                  imageUrl=""
-                  student={false}
-                  href=""
-                  className="w-full"
-                />
-              </div>
-
-              <div className="w-full self-stretch inline-flex justify-start items-center gap-5">
-                <InfoCard
-                  title={"UI/UX Designer"}
-                  subtitle={"IBM"}
-                  imageUrl=""
-                  student={false}
-                  href=""
-                  className="w-full"
-                />
-                <InfoCard
-                  title={"UI/UX Designer"}
-                  subtitle={"IBM"}
-                  imageUrl=""
-                  student={false}
-                  href=""
-                  className="w-full"
-                />
-              </div>
-
-              <div className="w-full self-stretch inline-flex justify-start items-center gap-5">
-                <InfoCard
-                  title={"UI/UX Designer"}
-                  subtitle={"IBM"}
-                  imageUrl=""
-                  student={false}
-                  href=""
-                  className="w-full"
-                />
-                <InfoCard
-                  title={"UI/UX Designer"}
-                  subtitle={"IBM"}
-                  imageUrl=""
-                  student={false}
-                  href=""
-                  className="w-full"
-                />
-              </div>
-
-              <div className="w-full self-stretch inline-flex justify-start items-center gap-5">
-                <InfoCard
-                  title={"UI/UX Designer"}
-                  subtitle={"IBM"}
-                  imageUrl=""
-                  student={false}
-                  href=""
-                  className="w-full"
-                />
-                <InfoCard
-                  title={"UI/UX Designer"}
-                  subtitle={"IBM"}
-                  imageUrl=""
-                  student={false}
-                  href=""
-                  className="w-full"
-                />
-              </div>
-
-              <div className="w-full self-stretch inline-flex justify-start items-center gap-5">
-                <InfoCard
-                  title={"UI/UX Designer"}
-                  subtitle={"IBM"}
-                  imageUrl=""
-                  student={false}
-                  href=""
-                  className="w-full"
-                />
-                <InfoCard
-                  title={"UI/UX Designer"}
-                  subtitle={"IBM"}
-                  imageUrl=""
-                  student={false}
-                  href=""
-                  className="w-full"
-                />
-              </div>
-
-              <div className="w-full self-stretch inline-flex justify-start items-center gap-5">
-                <InfoCard
-                  title={"UI/UX Designer"}
-                  subtitle={"IBM"}
-                  imageUrl=""
-                  student={false}
-                  href=""
-                  className="w-full"
-                />
-                <InfoCard
-                  title={"UI/UX Designer"}
-                  subtitle={"IBM"}
-                  imageUrl=""
-                  student={false}
-                  href=""
-                  className="w-full"
-                />
-              </div>
+            <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-5">
+              {fetchingOpportunities ? (
+                <Spinner />
+              ) : (
+                opportunities.slice(0, 12).map((opportunity) => (
+                  <InfoCard
+                    key={opportunity._id}
+                    title={opportunity.title}
+                    subtitle={opportunity.employer.name}
+                    imageUrl={opportunity.employer.profileImage || ""}
+                    student={false}
+                    href={`/opportunities/${opportunity._id}`}
+                    className="w-full"
+                  />
+                ))
+              )}
             </div>
           </div>
 
@@ -197,31 +99,22 @@ export default function Home() {
                 <div className="self-stretch justify-start text-zinc-300 text-sm font-medium leading-[150%]">Os talentos que se destacam na nossa comunidade</div>
               </div>
 
-              <div className="self-stretch inline-flex flex-col justify-start items-end gap-5">
-                <InfoCard
-                  title={"Júlia Leal"}
-                  subtitle={"Ciência da Computação"}
-                  imageUrl=""
-                  student
-                  href=""
-                  className="w-full"
-                />
-                <InfoCard
-                  title={"Rafael Facundo"}
-                  subtitle={"Filosofia"}
-                  imageUrl=""
-                  student
-                  href=""
-                  className="w-full"
-                />
-                <InfoCard
-                  title={"Gabriel Mota"}
-                  subtitle={"Ciência de Dados"}
-                  imageUrl=""
-                  student
-                  href=""
-                  className="w-full"
-                />
+              <div className="w-full self-stretch inline-flex flex-col justify-start items-center gap-5">
+                {fetchingStudents ? (
+                  <Spinner />
+                ) : (
+                  students.slice(0, 3).map((student) => (
+                    <InfoCard
+                      key={student._id}
+                      title={student.name}
+                      subtitle={student.course}
+                      imageUrl={student.profileImage || ""}
+                      student={true}
+                      href={`/students/${student._id}`}
+                      className="w-full"
+                    />
+                  ))
+                )}
               </div>
             </div>
 
@@ -231,23 +124,22 @@ export default function Home() {
                 <div className="self-stretch justify-start text-zinc-300 text-sm font-medium leading-[150%]">As maiores taxas de contratação no ConectaUFC</div>
               </div>
 
-              <div className="self-stretch inline-flex flex-col justify-start items-end gap-5">
-                <InfoCard
-                  title={"Insight Data Science Lab"}
-                  subtitle={"Laboratório de pesquisa em Ciência de Dados"}
-                  imageUrl=""
-                  student={false}
-                  href=""
-                  className="w-full"
-                />
-                <InfoCard
-                  title={"GREat"}
-                  subtitle={"Grupo de Redes de Computadores, Engenharia de Software"}
-                  imageUrl=""
-                  student={false}
-                  href=""
-                  className="w-full"
-                />
+              <div className="w-full self-stretch inline-flex flex-col justify-start items-center gap-5">
+                {fetchingEmployers ? (
+                  <Spinner />
+                ) : (
+                  employers.slice(0, 2).map((employer) => (
+                    <InfoCard
+                      key={employer._id}
+                      title={employer.name}
+                      subtitle={employer.name}
+                      imageUrl={employer.profileImage || ""}
+                      student={false}
+                      href={`/employers/${employer._id}`}
+                      className="w-full"
+                    />
+                  ))
+                )}
               </div>
             </div>
           </div>
